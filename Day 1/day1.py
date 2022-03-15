cbdccfe2c8ee3a82d64a080d915da13bb361bd92
HuckRob <RobertBuentgen@u.boisestate.edu>
2022-03-15 10:33:44 -0600

    Day 1 Part 1

---
+++ Day 1/day1.py
@@ -0,0 +1,6 @@
+#The Input() function behaves like the print() function but will
+#wait for the user to input some text before continuing
+
+#This will first ask the user "What is your name?"
+#Then it will print "Hello (user's inputted name)"
+print("Hello " + input("What is your name? "))
